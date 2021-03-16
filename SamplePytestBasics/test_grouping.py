3c98c14180bb5b56c7172604958d6b6866e650f7
B0222643 <b0222643@LTB0222643-MAC.local>
2021-03-16 17:27:39 +0530

    Batch file and report

--- SamplePytestBasics/test_grouping.py
+++ SamplePytestBasics/test_grouping.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import Chrome
 import pytest
 
+
 # Grouping is important because everytime we will not use the all test cases.
 # Sometimes we need to execute only particular TC so we use grouping for the same.
 # Can give Pytest.mark Somke/Sanity/Regression as pe the need. These are called as tag and can be anything
@@ -9,10 +10,10 @@ import pytest
 
 @pytest.mark.Smoke
 def test_conditional_valid_data():
-   path = "/Users/b0222643/node_modules/chromedriver/lib/chromedriver/chromedriver"
-   driver = Chrome(executable_path=path)
-   driver.get('http://www.thetestingworld.com/testings')
-   driver.maximize_window()
+    path = "/Users/b0222643/node_modules/chromedriver/lib/chromedriver/chromedriver"
+    driver = Chrome(executable_path=path)
+    driver.get('http://www.thetestingworld.com/testings')
+    driver.maximize_window()
 
 
 @pytest.mark.Sanity
@@ -21,10 +22,10 @@ def test_conditional_invalid_data():
     driver = Chrome(executable_path=path)
     driver.get('http://www.thetestingworld.com/testings')
 
+
 @pytest.mark.Smoke
 def test_registration_valid_data():
     path = "/Users/b0222643/node_modules/chromedriver/lib/chromedriver/chromedriver"
     driver = Chrome(executable_path=path)
     driver.get('http://www.thetestingworld.com/testings')
     driver.maximize_window()
-
